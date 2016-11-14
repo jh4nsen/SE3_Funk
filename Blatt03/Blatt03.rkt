4d56f65ce8d83732f100ff880d22bbcfc35c9f17
4jhansen <4jhansen@informatik.uni-hamburg.de>
2016-11-14 10:58:19 +0100

    NIEMAND!

--- Blatt03/Blatt03.rkt
+++ Blatt03/Blatt03.rkt
@@ -21,3 +21,22 @@
   )
 
 ;1.4
+(define (string->NATO in_string)
+  'keinbock
+  )
+
+(define (whyrecursion input)
+  (char->NATO (char->uppercase input))
+  )
+
+
+;2.1
+(require se3-bib/flaggen-module)
+(define flaggen (vector A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Z0 Z1 Z2 Z3 Z4 Z5 Z6 Z7 Z8 Z9))
+;siehe 1.1, einfach zu implementieren und addressieren
+
+;2.2
+(define (char->flagge in_char)
+  (cond [(char-alphabetic? in_char) (vector->values flaggen (- (char->integer in_char) 65) (- (char->integer in_char) 64))])
+  (cond [(char-numeric? in_char) (vector->values flaggen (- (char->integer in_char) 48) (- (char->integer in_char) 47))])
+  )
