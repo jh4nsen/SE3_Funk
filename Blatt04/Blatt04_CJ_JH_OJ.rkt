4e21a72a65e7a3b47c724a07af6c3f89a94bc68c
5janke <5janke@informatik.uni-hamburg.de>
2016-11-18 20:59:59 +0100

    Add files via upload
    Nr 2. 2 mehr auf die Aufgabe abgestimmt und 2.3 erstellt 2.1 muss noch iwie gemacht werden

--- Blatt04/Blatt04_CJ_JH_OJ.rkt
+++ Blatt04/Blatt04_CJ_JH_OJ.rkt
@@ -20,21 +20,31 @@
 (require se3-bib/tools-module)
 ;Aufgabe 2
 (define (headline shipname rufzeichen)
-  (display (string-append "MAYDAY MAYDAY MAYDAY\n" (string->NATOStr "DE") shipname " " shipname " " shipname " " (string->NATOStr rufzeichen)
-                          "\nMAYDAY " shipname " " (string->NATOStr shipname) "\nIch buchstabiere " (string->NATOStr rufzeichen) "\n")))
-(define (place)
-  (display (string-append "Notfallposition:"(car(one-of '(" Bermuda Dreieck\n" " Leuchtturm Kiel\n" " Kap hoorn\n" " Freibad\n"))))))
+  (display (string-append "MAYDAY MAYDAY MAYDAY\n" (string->NATOStr "DE") "\n" shipname " " shipname " " shipname " " (string->NATOStr rufzeichen)
+                          "\nMAYDAY " shipname " Ich buchstabiere " (string->NATOStr shipname) "\nRufzeichen " (string->NATOStr rufzeichen) "\n")))
+(define (place ort)
+  (display (string-append "Notfallposition " ort "\n")))
 
 (define (emergency artNot)
-  (display (string-append "Wir haben einen Notfall! " artNot " ")))
+  (display (string-append "Wir haben einen Notfall \n" artNot "\n")))
 
 (define (furtherInstr hilfeLeist)
-  (cond [(non-empty-string? hilfeLeist) (display "Sonst keine Probleme.")]
-        [(display  (string-append "Wir benötigen folgende Hilfeleistungen" hilfeLeist))]))
+  (cond [(non-empty-string? hilfeLeist)(display string-append "Wir benötigen folgende Hilfeleistungen \n" hilfeLeist)]
+        [(display "Keine weiteren Angaben zu Hilfeleistungen")]))
 
 (define (signature shipname rufzeichen)
   (display (string-append shipname " " (string->NATOStr rufzeichen))))
 
-(define (notfallsignal shipname rufzeichen artNot hilfeLeist)
+(define (notfallsignal shipname rufzeichen ort artNot hilfeLeist)
   (headline shipname rufzeichen)
-  (place)(emergency artNot)(furtherInstr hilfeLeist)(display "\n----\n")(signature shipname rufzeichen)(display "\nOVER"))
+  (place ort)(emergency artNot)(furtherInstr hilfeLeist)(display " ----\n")(signature shipname rufzeichen)(display "\nOVER"))
+
+;;Tests zu Seaside und Amira :
+(define (test shipname)
+  (cond [(string=? shipname "SEASIDE")(notfallsignal "SEASIDE" "SSDE" "UNGEFÄHR 10 SM NORDÖSTLICH LEUCHTTURM KIEL"
+               "NOTFALLZEIT 1000 UTC \nSCHWERER WASSEREINBRUCH WIR SINKEN\nKEINE VERLETZTEN\nVIER MANN GEHEN IN DIE RETTUNGSINSEL"
+               "SCHNELLE HILFE ERFORDERLICH\nICH SENDE DEN TRÄGER")])
+  (cond [(string=? shipname "AMIRA")(notfallsignal "AMIRA" "AMRY" "57°46’N, 006°31’E"
+               "NOTFALLZEIT 0640 UTC UTC \nKENTERUNG IN SCHWERER SEE\nNEUN MANN AN BORD\nDAS SCHIFF IST 15M LANG UND HAT EINEN GRÜNEN RUMPF"
+               "")]))
+
